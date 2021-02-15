71fdeffa93f9bfd1d7821ec0a0338af851fcc9ae
Mai <hungry.mai@hotmail.com>
2021-02-15 20:16:42 +0700

    แนะนำโรค v1

---
+++ finalproject_1/finalproject_1/Model/DiseaseIntrodction.swift
@@ -0,0 +1,17 @@
+//
+//  DiseaseIntrodction.swift
+//  finalproject_1
+//
+//  Created by Mai on 15/2/2564 BE.
+//  Copyright © 2564 BE ICTSUIM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+struct Diseaseintrodction {
+    var name: String?
+    let image: UIImage?
+}
